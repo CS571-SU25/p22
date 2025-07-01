8c68e834022c42a931177aeaa0feda7cc9d3c1f9
davgreenberg <dgreenberg8@wisc.edu>
2025-07-01 13:43:00 -0400

    Removed default page items, added ActivityCard component for activities and activityData.json, to be used to dynamically generate ActivityCards

---
+++ src/components/ActivityCard.jsx
@@ -0,0 +1,20 @@
+import { Card, Fade } from "react-bootstrap";
+import "./ActivityCard.css"
+import { useEffect, useState } from "react";
+
+export default function ActivityCard({name, img, desc, delay}) {
+    const [show, setShow] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => setShow(true), delay)
+    }, [])
+
+    return <Card className={`activity-card ${show ? "show" : ""}`} style={{minWidth: "300px", position: "relative"}} onClick={() => {location.href = `/p22/${name}`}}>
+            <div style={{backgroundImage: `url(${img.src})`}}>
+                <h2>{name}</h2>
+            </div>
+            <p style={{margin:"auto", padding: "20px"}}>
+                {desc}
+            </p>
+        </Card>
+}
