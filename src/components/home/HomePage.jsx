17387052acd241c5ea275ed220f5e17c46743173
davgreenberg <dgreenberg8@wisc.edu>
2025-08-04 20:01:13 -0400

    rebuilt app

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -2,7 +2,7 @@ import CurrentWeather from "./CurrentWeather"
 
 export default function HomePage({ weather, loc, units }) {
     return <>
-        <h1>Welcome to the Activity Generator</h1>
+        <h1 style={{margin: 20}}>Welcome to the Activity Generator</h1>
         <CurrentWeather weather={weather} loc={loc} units={units} />
     </> 
 }
