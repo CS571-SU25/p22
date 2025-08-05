9f6cf099c13f4c838c3d0f300a51e9a6b099f980
davgreenberg <dgreenberg8@wisc.edu>
2025-08-04 21:38:10 -0400

    think it was the api key... oops

--- src/tools/fetchLocation.js
+++ src/tools/fetchLocation.js
@@ -21,11 +21,11 @@ export default async function fetchLocation() {
             const res = await fetch("https://api.ipify.org");
             const ip = await res.text();
 
-            const locRes = await fetch(`https://api.ipgeolocation.io/v2/ipgeo?apiKey=a858d786813e46279445d41f5aad29d2&ip=${ip}`);
+            const locRes = await fetch(`http://ipwho.is/${ip}`);
             const data = await locRes.json();
-            console.log(data.location.latitude, data.location.longitude);
+            console.log(data.latitude, data.longitude);
 
-            return { lat: data.location.latitude, long: data.location.longitude };
+            return { lat: data.latitude, long: data.longitude };
         } catch (fallbackErr) {
             console.error("Failed to fetch IP-based location:", fallbackErr);
             return null;
